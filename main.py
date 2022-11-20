9c8469cba46dc61bd6644bb176949120af77632e
Adam <adamb56789@gmail.com>
2022-11-20 16:22:49 +0000

    added umqtt library

--- main.py
+++ main.py
@@ -5,39 +5,16 @@ import dht, time
 led = Pin("LED", Pin.OUT)
 def blink(duration):
     led.on()
-    sleep_ms(duration)
+    sleep_ms(10)
     led.off()
-    sleep_ms(duration)
+    sleep_ms(200)
 
-def led_morse(message):
-    DOT = 10
-    MORSE_CODE_DICT = {'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.','F':'..-.', 'G':'--.', 'H':'....','I':'..', 'J':'.---', 'K':'-.-','L':'.-..', 'M':'--', 'N':'-.','O':'---', 'P':'.--.', 'Q':'--.-','R':'.-.', 'S':'...', 'T':'-','U':'..-', 'V':'...-', 'W':'.--','X':'-..-', 'Y':'-.--', 'Z':'--..','1':'.----', '2':'..---', '3':'...--','4':'....-', '5':'.....', '6':'-....','7':'--...', '8':'---..', '9':'----.','0':'-----', ', ':'--..--', '.':'.-.-.-','?':'..--..', '/':'-..-.', '-':'-....-','(':'-.--.', ')':'-.--.-'}
-    morse = ''
-    for letter in message.upper():
-        if letter != ' ':
-            morse += MORSE_CODE_DICT[letter] + ' '
-        else:
-            morse += '/ '
+dht = dht.DHT22(Pin(0))
 
-    
-    for c in morse:
-        if c == ".":
-            blink(DOT)
-        elif c == "-":
-            blink(DOT * 3)
-        elif c == " ":
-            sleep_ms(DOT * 3)
-        elif c == "/":
-            sleep_ms(DOT)
-
-# led_morse("Hello World")
-
-dht = dht.DHT22(Pin(2))
-while True:
-    dht.measure()
-    temperature = dht.temperature()
-    humidity = dht.humidity()
-    print('Temperature:', temperature, 'C    Humidity:', humidity, '%')
-    for i in range(temperature - 15.99):
-        blink(100)
-    sleep(2)
+dht.measure()
+temperature = dht.temperature()
+humidity = dht.humidity()
+print('Temperature:', temperature, 'C    Humidity:', humidity, '%')
+for i in range(temperature - 15.99):
+    blink(100)
+sleep(2)
